183293f40dcbc40893d27c890b9b4ed086fb2cb1
doniaskima <doniaskima344@gmail.com>
2022-08-13 10:32:25 +0100

    nav

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,14 +1,25 @@
 import React from 'react'
 import "./Header.css";
+import SearchIcon from "@material-ui/icons/Search";
+import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
 import { Link } from "react-router-dom";
 const Header = () => {
     return (
         <div className="header">
+
             <img
                 className="header__logo"
                 src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
             />
+            <div className="headerSearch">
+                <input className="searchInput" type="text" />
+                <SearchIcon className="searchIcon" />
+            </div>
+
+
         </div>
+
+
     )
 }
 
